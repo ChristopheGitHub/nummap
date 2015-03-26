2214c74581aecf0f1d5518f98193971e10ac756e
Christophe <christophe.guillonnet@gmail.com>
2015-03-26 17:35:26 +0100

    Design carte (Thx Marc CSS Consulting)

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -6,4 +6,26 @@ angular.module('nummapApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        var tilesDict = {
+            openstreetmap: {
+                url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                options: {
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }
+            }
+        };
+
+        angular.extend($scope, {
+            center: {
+                lat: 43.28520334369384,
+                lng: -0.289764404296875,
+                zoom: 9
+            },
+            tiles: tilesDict.openstreetmap,
+            defaults: {
+                scrollWheelZoom: false,
+                zoomControlPosition: 'bottomleft'
+            }
+        });
     });
