5e42ae66c4fe2704b46033f21330945e42420762
Christophe <christophe.guillonnet@gmail.com>
2015-04-02 20:09:34 +0200

    Correction d'erreurs sur fields et sector

--- src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -1,33 +1,76 @@
 'use strict';
 
 angular.module('nummapApp')
-    .controller('SettingsController', function ($scope, Principal, Auth) {
+    .controller('SettingsController', function ($scope, Principal, Auth, Domains, Competencies) {
         $scope.success = null;
         $scope.error = null;
         Principal.identity().then(function(account) {
             $scope.settingsAccount = account;
-
-            // Pour permettre l'affichage des listes de réseaux sociaux
-            $scope.personSocialNetworkList = $scope.settingsAccount.personContactInformation.socialNetworkList ;
-            if($scope.category === 'COMPANY') {
-                $scope.companySocialNetworkList = $scope.settingsAccount.companyContactInformation.socialNetworkList ;
-            }
-
+            console.log('settings');
             console.log($scope.settingsAccount);
+            
+            Domains.query(function(result) {
+                $scope.sectors = result;
+                console.log('sectors');
+                console.log($scope.sectors);
+                
+                // Valeurs déjà choisies
+                $scope.sectors.forEach(function (element) {
+                    if ($.inArray(element.name, $scope.settingsAccount.sectors) !== -1) {
+                        element.checked = true;
+                    }
+                });
+            });
+        
+            Competencies.query(function(result) {
+                $scope.competencies = result;
+                // $scope.competencies
+            });
+
+            $scope.fields = [
+                {name: 'Outsourcing', value: 'OUTSOURCING'},
+                {name: 'Consulting', value: 'CONSULTING'},
+                {name: 'System Integration', value: 'SYSTEM_INTEGRATION'}
+            ];
+
+            $scope.fields.forEach(function (element) {
+                if ($.inArray(element.value, $scope.settingsAccount.fields) !== -1) {
+                    element.checked = true;
+                } else {
+                    element.checked = false;
+                }
+                console.log(element.name +' : ' + element.checked);
+            });
         });
 
 
+        
+        $scope.categories = [
+            {value: 'STUDENT', translationKey: 'register.form.category.student'},
+            {value: 'PROFESSOR', translationKey: 'register.form.category.professor'},
+            {value: 'FREELANCE', translationKey: 'register.form.category.freelance'},
+            {value: 'COMPANY', translationKey: 'register.form.category.company'},
+            {value: 'ASSOCIATION', translationKey: 'register.form.category.association'}
+        ];
 
-            $scope.save = function () {
-                Auth.updateAccount($scope.settingsAccount).then(function() {
-                    $scope.error = null;
-                    $scope.success = 'OK';
-                    Principal.identity().then(function(account) {
-                        $scope.settingsAccount = account;
-                    });
-                }).catch(function() {
-                    $scope.success = null;
-                    $scope.error = 'ERROR';
+        
+
+
+
+        console.log($scope.settingsAccount);
+
+
+
+        $scope.save = function () {
+            Auth.updateAccount($scope.settingsAccount).then(function() {
+                $scope.error = null;
+                $scope.success = 'OK';
+                Principal.identity().then(function(account) {
+                    $scope.settingsAccount = account;
                 });
-            };
+            }).catch(function() {
+                $scope.success = null;
+                $scope.error = 'ERROR';
+            });
+        };
     });
