f84194754693dd557cf03b6687036127b408189f
Christophe <christophe.guillonnet@gmail.com>
2015-04-03 15:58:27 +0200

    Transformation des fenetres modals en ui-view

--- src/main/webapp/scripts/app/admin/users/user-detail.controller.js
+++ src/main/webapp/scripts/app/admin/users/user-detail.controller.js
@@ -9,10 +9,25 @@
 angular.module('nummapApp')
     .controller('UserDetailController', function ($scope, $stateParams, User) {
         $scope.user = {};
+        // $scope.user = {};
         $scope.load = function (login) {
             User.get({login: login}, function(result) {
                 $scope.user = result;
             });
         };
         $scope.load($stateParams.login);
+
+        $scope.categories = [
+            {value: 'STUDENT', translationKey: 'register.form.category.student'},
+            {value: 'PROFESSOR', translationKey: 'register.form.category.professor'},
+            {value: 'FREELANCE', translationKey: 'register.form.category.freelance'},
+            {value: 'COMPANY', translationKey: 'register.form.category.company'},
+            {value: 'ASSOCIATION', translationKey: 'register.form.category.association'}
+        ];
+
+        $scope.fields = [
+            {name: 'Outsourcing', value: 'OUTSOURCING'},
+            {name: 'Consulting', value: 'CONSULTING'},
+            {name: 'System Integration', value: 'SYSTEM_INTEGRATION'}
+        ];
     });
