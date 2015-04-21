1c82934d11f4353e8f3eef97c57eec4df7587ea2
Louis Pouliquen <pouliquenl@eisti.eu>
2015-04-21 14:20:37 +0200

    Upload

---
+++ src/main/webapp/scripts/app/admin/csvImport/csvImport.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('nummapApp')
+    .controller('csvImportController', ['$scope', '$upload', function ($scope, $upload) {
+        $scope.$watch('files', function () {
+            $scope.upload($scope.files);
+        });
+
+        $scope.upload = function (files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    $upload.upload({
+                        url: '/api/uploadcsv',
+                        fields: {'name': file.name},
+                        file: file
+                    }).progress(function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                    }).success(function (data, status, headers, config) {
+                        console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                    });
+                }
+            }
+        };
+    }]);
