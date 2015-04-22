df5cf67cf0532a18e57eb60facfd4cd3f71da86a
Louis Pouliquen <pouliquenl@eisti.eu>
2015-04-22 09:13:31 +0200

    Parsingé

--- src/main/webapp/scripts/app/admin/csvImport/csvImport.controller.js
+++ src/main/webapp/scripts/app/admin/csvImport/csvImport.controller.js
@@ -5,6 +5,7 @@ angular.module('nummapApp')
         $scope.$watch('files', function () {
             $scope.upload($scope.files);
         });
+        $scope.success = false;
 
         $scope.upload = function (files) {
             if (files && files.length) {
@@ -18,9 +19,13 @@ angular.module('nummapApp')
                         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                         console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                     }).success(function (data, status, headers, config) {
+                        $scope.success = true;
                         console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-                    });
-                }
+                    }).
+                        error(function(data, status, headers, config) {
+                        $scope.success = false;
+                  });;
+                  }
             }
         };
     }]);
